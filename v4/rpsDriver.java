5d8a30a73307694c819783df4db2909a5f0e4221
brianwang30 <bwang30@stuy.edu>
2022-06-01 12:34:39 -0400

    emergency

---
+++ v4/rpsDriver.java
@@ -0,0 +1,20 @@
+
+public class rpsDriver{
+
+    public static void main(String[] args) {
+      rps newGame = new rps();
+
+      newGame.game();
+      System.out.println(newGame.getMoveHistory());
+      System.out.println("Games played: " + newGame.getTurn());
+      double pWinPercent = newGame.getPScore() * 1.0 / newGame.getTurn();
+      double gWinPercent = newGame.getGScore() * 1.0 / newGame.getTurn();
+      System.out.println("Player score: " + newGame.getPScore() + " (Winrate: " + pWinPercent + "%)");
+      System.out.println("Enkidu score: " + newGame.getGScore() + " (Winrate: " + gWinPercent + "%)");
+      System.out.println("Ties: " + newGame.getTie());
+    }
+
+
+
+
+}
