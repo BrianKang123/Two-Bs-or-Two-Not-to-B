4cb3cee083e14c436b6e551a3930b4385f0d847d
Ethan Lam <elam20@stuy.edu>
2022-06-02 12:30:03 -0400

    testing

--- v1/rpsDriver.java
+++ v1/rpsDriver.java
@@ -3,17 +3,14 @@ public class rpsDriver{
 
     public static void main(String[] args) {
       rps newGame = new rps();
-      /*
-      for(int i = 0 ; i < args.length ; i++){
-        newGame.turn(Integer.parseInt(args[i]));
 
-      }
-      */
       newGame.game();
       System.out.println(newGame.getMoveHistory());
       System.out.println("Games played: " + newGame.getTurn());
-      System.out.println("Player score: " + newGame.getPScore());
-      System.out.println("Gilgamesh score: " + newGame.getGScore());
+      double pWinPercent = newGame.getPScore() * 1.0 / newGame.getTurn();
+      double gWinPercent = newGame.getGScore() * 1.0 / newGame.getTurn();
+      System.out.println("Player score: " + newGame.getPScore() + " (Winrate: " + pWinPercent + "%)");
+      System.out.println("Maurice score: " + newGame.getGScore() + " (Winrate: " + gWinPercent + "%)");
       System.out.println("Ties: " + newGame.getTie());
     }
 
@@ -21,3 +18,4 @@ public class rpsDriver{
 
 
 }
+
